64cd1a30a3ab0a4b4c4a04430f00f5f560fb296a
Andrea Borgia <andrea@borgia.bo.it>
2026-06-05 16:16:17 +0200

    at end of 3.1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use leptos::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    mount_to_body(App);
+}
+
+#[component]
+fn App() -> impl IntoView {
+    let (count, set_count) = signal(0);
+
+    view! {
+        <button // define an event listener with on:
+        on:click=move |_| {
+            set_count.set(3)
+        }>
+            // text nodes are wrapped in quotation marks
+            "Click me: "
+            // blocks include Rust code
+            // in this case, it renders the value of the signal
+            {count}
+        </button>
+        <p>"Double count: " {move || count.get() * 2}</p>
+    }
 }
