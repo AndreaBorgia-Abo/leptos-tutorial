fb6f525480b92e4b9ada99f0a951b356cfaafd10
Andrea Borgia <andrea@borgia.bo.it>
2026-07-17 16:22:10 +0200

    at 3.5 after option 4 (stores)

--- src/main.rs
+++ src/main.rs
@@ -134,7 +134,7 @@ pub fn App() -> impl IntoView {
 } */
 
 // Option 3 - using Memo
-#[derive(Debug, Clone)]
+/* #[derive(Debug, Clone)]
 struct DatabaseEntry {
     key: String,
     value: i32,
@@ -178,6 +178,63 @@ pub fn App() -> impl IntoView {
                     data.with(|data| data.get(index.get()).map(|d| d.value).unwrap_or(0))
                 });
                 view! { <p>{value}</p> }
+            }
+        />
+    }
+}
+*/
+
+use reactive_stores::Store;
+
+#[derive(Store, Debug, Clone)]
+pub struct Data {
+    #[store(key: String = |row| row.key.clone())]
+    rows: Vec<DatabaseEntry>,
+}
+
+#[derive(Store, Debug, Clone)]
+struct DatabaseEntry {
+    key: String,
+    value: i32,
+}
+
+#[component]
+pub fn App() -> impl IntoView {
+    // instead of a signal with the rows, we create a store for Data
+    let data = Store::new(Data {
+        rows: vec![
+            DatabaseEntry {
+                key: "foo".to_string(),
+                value: 10,
+            },
+            DatabaseEntry {
+                key: "bar".to_string(),
+                value: 20,
+            },
+            DatabaseEntry {
+                key: "baz".to_string(),
+                value: 15,
+            },
+        ],
+    });
+
+    view! {
+        // when we click, update each row,
+        // doubling its value
+        <button on:click=move |_| {
+            use reactive_stores::StoreFieldIterator;
+            for row in data.rows().iter_unkeyed() {
+                *row.value().write() *= 2;
+            }
+            leptos::logging::log!("{:?}", data.get());
+        }>"Update Values"</button>
+        // iterate over the rows and display each value
+        <For
+            each=move || data.rows()
+            key=|row| row.read().key.clone()
+            children=|child| {
+                let value = child.value();
+                view! { <p>{move || value.get()}</p> }
             }
         />
     }
