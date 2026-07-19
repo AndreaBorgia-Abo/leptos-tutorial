6cef0cb984b594a5e5b03e5ad3d8f42fdb44f5f6
Andrea Borgia <andrea@borgia.bo.it>
2026-07-19 17:16:59 +0200

    at 3.6 after controlled inputs

--- src/main.rs
+++ src/main.rs
@@ -349,5 +349,35 @@ fn DynamicList(
 }
 #[component]
 pub fn App() -> impl IntoView {
-    view! { <DynamicList initial_length=3 /> }
+    let (name, set_name) = signal("Name".to_string());
+    let email = RwSignal::new("".to_string());
+    let favorite_color = RwSignal::new("red".to_string());
+    let spam_me = RwSignal::new(true);
+
+    view! {
+        <DynamicList initial_length=3 />
+        <input type="text" bind:value=(name, set_name) />
+        <input type="email" bind:value=email />
+        <label>
+            "Please send me lots of spam email." <input type="checkbox" bind:checked=spam_me />
+        </label>
+        <fieldset>
+            <legend>"Favorite color"</legend>
+            <label>
+                "Red" <input type="radio" name="color" value="red" bind:group=favorite_color />
+            </label>
+            <label>
+                "Green" <input type="radio" name="color" value="green" bind:group=favorite_color />
+            </label>
+            <label>
+                "Blue" <input type="radio" name="color" value="blue" bind:group=favorite_color />
+            </label>
+        </fieldset>
+        <p>"Your favorite color is " {favorite_color} "."</p>
+        <p>"Name is: " {name}</p>
+        <p>"Email is: " {email}</p>
+        <Show when=move || spam_me.get()>
+            <p>"You’ll receive cool bonus content!"</p>
+        </Show>
+    }
 }
